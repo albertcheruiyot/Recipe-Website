5fa052c1bd2aa795225c02615ed414e634c6d12e
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-11 23:11:58 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# Recipe-Website
+# Recipe Website
+
+Welcome to our Recipe Website! This repository contains the source code for a platform where you can explore and discover delicious recipes from around the world. Whether you're a seasoned chef or just starting out in the kitchen, we hope you find inspiration and joy in cooking and trying out new recipes.
+
+## Features
+
+- Browse a vast collection of recipes.
+- Engage with the community by rating your cooking experiences.
+- Responsive design, allowing for a seamless experience across all platforms.
+
+## Getting Started
+
+To clone this project and run it locally on your machine, follow these steps:
+
+### 1. Install Node.js and npm
+
+If you haven't installed Node.js and npm yet, you need to do that first. Follow the instructions below for your operating system:
+- **Ubuntu**:
+```sh
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+```
+
+
+### 2. Clone the repository
+
+Open a terminal and run the following command:
+
+```sh
+git clone https://github.com/albertcheruiyot/Recipe-Website.git
+```
+
+
+### 3. Navigate to the project directory
+
+Change into the project directory:
+
+```sh
+cd recipe-website
+```
+
+
+### 4. Install dependencies
+
+Install the necessary dependencies by running:
+
+```sh
+npm install
+```
+
+
+### 5. Start the JSON server
+
+Before starting the development server, you need to start the JSON server. Navigate to the directory containing `recipe.json` and run:
+
+
+```sh
+json-server --watch recipe.json
+```
+
+
+### 6. Start the development server
+
+Once the dependencies are installed and the JSON server is running, start the development server by running:
+
+```sh
+npm start
+```
+
+
+### 7. Access the website
+
+Open your web browser and navigate to `http://localhost:3000` to view the Recipe Website.
+
+## Contributing
+
+I welcome contributions from the community to make our Recipe Website even better! If you have ideas for new features, improvements, or bug fixes, please feel free to submit a pull request. Make sure to follow our [contributing guidelines](CONTRIBUTING.md) before getting started.
+
+## Feedback and Support
+
+If you encounter any issues while using our Recipe Website or have suggestions for improvement, please don't hesitate to [open an issue](https://github.com/albertcheruiyot/Recipe-Website/issues) on GitHub. Your feedback is invaluable to us as we strive to create a delightful cooking experience for our users.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- index.js
+++ index.js
@@ -1,150 +1,198 @@
-
 // Fetch the recipes
 var player;
-function fetchAllRecipes() { // Function to fetch comments
-  fetch("http://localhost:3000/recipes")
-    .then(response => response.json())
-    .then(data => {
-      const recipeContainer = document.getElementById('recipeContainer');
-      //console.log(data);
-      // Iterate through the recipes and create divs for each
-      data.forEach(recipe => {
+var totalLikes, totalDislikes;
+var likeParapgraph;
+var unlikeParapgraph;
 
-        let recipeID = recipe.id;
-        //console.log(recipeID);
-        console.log(recipeID);
-        const recipeDiv = document.createElement('div');
-        recipeDiv.classList.add('recipe');
+document.addEventListener("DOMContentLoaded", function() {
+  // Fetch the recipes
+  function fetchAllRecipes() {
+      fetch("http://localhost:3000/recipes")
+          .then(response => response.json())
+          .then(data => {
+              const recipeContainer = document.getElementById('recipeContainer');
+              data.forEach(recipe => {
+                  let recipeID = recipe.id;
+                  const recipeDiv = document.createElement('div');
+                  recipeDiv.classList.add('recipe');
+                  const titleElement = document.createElement('h2');
+                  titleElement.textContent = recipe.title;
+                  const imageElement = document.createElement('img');
+                  imageElement.src = recipe.image;
+                  imageElement.alt = recipe.title;
+                  const groupDiv = document.createElement('div');
+                  groupDiv.classList.add('group-div');
+                  const imageDiv = document.createElement('div');
+                  imageDiv.classList.add('image-div');
+                  const likesDiv = document.createElement('div');
+                  likesDiv.classList.add('likes-div');
+                  const likesElement = document.createElement('h3');
+                  let likes = recipe.likes;
+                  if (likes == 1) {
+                      likesElement.textContent = `${likes} person Liked this recipe. Try it!!!`;
+                  } else if (likes > 1) {
+                      likesElement.textContent = `${likes} people Liked this recipe. Try it!!!`;
+                  } else {
+                      likesElement.textContent = `Be the first one to try this recipe.`;
+                  }
+                  const button = document.createElement('button');
+                  button.classList.add('button-82-pushable');
+                  button.setAttribute('role', 'button');
+                  const shadowSpan = document.createElement('span');
+                  shadowSpan.classList.add('button-82-shadow');
+                  const edgeSpan = document.createElement('span');
+                  edgeSpan.classList.add('button-82-edge');
+                  const frontSpan = document.createElement('span');
+                  frontSpan.classList.add('button-82-front', 'text');
+                  frontSpan.textContent = 'Start Cooking This Food';
+                  button.appendChild(shadowSpan);
+                  button.appendChild(edgeSpan);
+                  button.appendChild(frontSpan);
+                  const descriptionElement = document.createElement('p');
+                  descriptionElement.textContent = recipe.description;
+                  recipeDiv.appendChild(titleElement);
+                  recipeDiv.appendChild(groupDiv);
+                  groupDiv.appendChild(imageDiv);
+                  groupDiv.appendChild(likesDiv);
+                  likesDiv.appendChild(descriptionElement);
+                  likesDiv.appendChild(likesElement);
+                  likesDiv.appendChild(button);
+                  imageDiv.appendChild(imageElement);
+                  recipeContainer.appendChild(recipeDiv);
+                  button.addEventListener('click', function () {
+                      fetchRecipeByID(recipeID);
+                  });
+              });
+          })
+          .catch(error => console.error("Error fetching recipes:", error));
+  }
 
-        // Create title element
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = recipe.title;
+  function fetchRecipeByID(recipeID) {
+      fetch(`http://localhost:3000/recipes/${recipeID}`)
+          .then(response => response.json())
+          .then(data => {
+              const recipeTitle = document.getElementById('recipe-title');
+              recipeTitle.textContent = data.title;
+              const recipeDescription = document.getElementById('recipe-description');
+              recipeDescription.textContent = data.description;
+              const ingredientList = document.querySelector("#ingredients_list");
+              ingredientList.innerHTML = "";
+              data.ingredients.forEach(ingredient => {
+                  const listItem = document.createElement("li");
+                  listItem.textContent = ingredient;
+                  ingredientList.appendChild(listItem);
+              });
+              const instructionList = document.querySelector("#instructions_list");
+              instructionList.innerHTML = "";
+              data.instructions.forEach(instruction => {
+                  const listItem = document.createElement("li");
+                  listItem.textContent = instruction;
+                  instructionList.appendChild(listItem);
+              });
+              totalLikes = data.likes;
+              totalDislikes = data.dislikes;
+              likeParapgraph = document.getElementById('likes-text');
+              unlikeParapgraph = document.getElementById('unlikes-text');
+              likeParapgraph.textContent = `${data.likes} likes`;
+              unlikeParapgraph.textContent = `${data.dislikes} dislikes`;
+              playVideo(data.video);
+          })
+          .catch(error => console.error("Error fetching recipes:", error));
+      scrollToRecipe();
+      giveFeedback();
+  }
 
-        // Create image element
-        const imageElement = document.createElement('img');
-        imageElement.src = recipe.image;
-        imageElement.alt = recipe.title;
-
-        const groupDiv = document.createElement('div');
-        groupDiv.classList.add('group-div');
-        const imageDiv = document.createElement('div');
-        imageDiv.classList.add('image-div');
-        const likesDiv = document.createElement('div');
-        likesDiv.classList.add('likes-div');
-
-
-        const likesElement = document.createElement('h3');
-        let likes = recipe.likes;
-        if (likes == 1) {
-          likesElement.textContent = `${likes} person Liked this recipe. Try it!!!`;
-        } else if (likes > 1) {
-          likesElement.textContent = `${likes} people Liked this recipe. Try it!!!`;
-        } else {
-          likesElement.textContent = `Be the first one to try this recipe.`;
-        }
-        /*const dislikesElement = document.createElement('h3');
-        dislikesElement.textContent = `${recipe.dislikes} likes`;*/
-
-        // Create button element
-        const button = document.createElement('button');
-        button.classList.add('button-82-pushable');
-        button.setAttribute('role', 'button');
-
-        // Create span elements
-        const shadowSpan = document.createElement('span');
-        shadowSpan.classList.add('button-82-shadow');
-
-        const edgeSpan = document.createElement('span');
-        edgeSpan.classList.add('button-82-edge');
-
-        const frontSpan = document.createElement('span');
-        frontSpan.classList.add('button-82-front', 'text');
-        frontSpan.textContent = 'Start Cooking This Food';
-
-        // Append span elements to button element
-        button.appendChild(shadowSpan);
-        button.appendChild(edgeSpan);
-        button.appendChild(frontSpan);
-
-        // Create description element
-        const descriptionElement = document.createElement('p');
-        descriptionElement.textContent = recipe.description;
-
-        // Append title and description to recipe div
-        recipeDiv.appendChild(titleElement);
-        recipeDiv.appendChild(groupDiv);
-        groupDiv.appendChild(imageDiv);
-        groupDiv.appendChild(likesDiv);
-        likesDiv.appendChild(descriptionElement);
-        likesDiv.appendChild(likesElement);
-        likesDiv.appendChild(button);
-        imageDiv.appendChild(imageElement);
-
-        // Append recipe div to container
-        recipeContainer.appendChild(recipeDiv);
-
-        // Add onclick listener to the button
-        button.addEventListener('click', function () {
-          // Add your desired functionality here
-          fetchRecipeByID(recipeID);
-        });
-
-
+  function giveFeedback(){
+      const LikeButton = document.getElementById('like-recipe');
+      const unlikeButton = document.getElementById('unlike-recipe');
+      LikeButton.addEventListener('click', function () {
+          // Add your logic for handling like feedback here
+          newLikes = totalLikes + 1;
+          console.log(newLikes);
+          likeParapgraph.textContent = `${newLikes} likes`;
+          
       });
-    })
-    .catch(error => console.error("Error fetching recipes:", error));
+      unlikeButton.addEventListener('click', function () {
+          // Add your logic for handling unlike feedback here
+          newDislikes = totalDislikes + 1;
+          console.log(newDislikes);
+          unlikeParapgraph.textContent = `${newDislikes} dislikes`;
+      });
+  }
+
+  var box1 = document.getElementById('box1');
+  var box2 = document.getElementById('box2');
+
+  function syncScroll(event) {
+      if (event.target === box1) {
+          box2.scrollTop = box1.scrollTop;
+      } else if (event.target === box2) {
+          box1.scrollTop = box2.scrollTop;
+      }
+  }
+
+  var navLinks = document.querySelectorAll('.nav-links a');
+  navLinks.forEach(function (navLink) {
+      navLink.addEventListener('click', function (e) {
+          e.preventDefault(); // Prevent default anchor behavior
+          var targetId = this.getAttribute('href'); // Get target section ID
+          var targetSection = document.querySelector(targetId); // Get target section element
+          if (targetSection) {
+              // Scroll to the target section with smooth behavior
+              targetSection.scrollIntoView({ behavior: 'smooth' });
+          }
+      });
+  });
+
+  fetchAllRecipes();
+  fetchRecipeByID(4);
+});
+
+function scrollToRecipe() {
+  var sectionToScrollTo = document.getElementById('about-section');
+  sectionToScrollTo.scrollIntoView({ behavior: 'smooth' });
 }
 
-fetchAllRecipes();
-fetchRecipeByID(4);
-function fetchRecipeByID(recipeID){
-  fetch(`http://localhost:3000/recipes/${recipeID}`)
-    .then(response => response.json())
-    .then(data => {
-  
-      //console.log(data.title)
-      const recipeTitle = document.getElementById('recipe-title');
-      recipeTitle.textContent = data.title;
-      const recipeDescription = document.getElementById('recipe-description');
-      recipeDescription.textContent = data.description;
 
-      const ingredientList = document.querySelector("#ingredients_list");
-        ingredientList.innerHTML = "";
+// Initialize YouTube Player
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: '', // Default video ID
+        playerVars: {
+            'autoplay': 0, // Disable autoplay
+            'controls': 1, // Enable player controls
+            'rel': 1, // Disable related videos
+            'showinfo': 0, // Hide video title and uploader info
+            'modestbranding': 1 // Hide YouTube logo
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onError': onPlayerError
+        }
+    });
+}
 
-      data.ingredients.forEach(ingredient => {
-      
-        const listItem = document.createElement("li");
-        
-  
-        listItem.textContent = ingredient;
-       
-       
-        ingredientList.appendChild(listItem);
-      });
-      const instructionList = document.querySelector("#instructions_list");
-        instructionList.innerHTML = "";
-        
-      data.instructions.forEach(instruction=> {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = "";
-  
-        listItem.textContent = instruction;
-    
-       
-        instructionList.appendChild(listItem);
-      });
-      // Iterate through the recipes and create divs for each
-      /*data.forEach(recipe => {
-        const recipeTitle = document.getElementById('recipe-title');
-        recipeTitle.textContent = recipe.title;
+// Function to execute when the player is ready
+function onPlayerReady(event) {
+    // You can do something when the player is ready here
+}
 
-      });*/
+// Function to handle player errors
+function onPlayerError(event) {
+    // If an error occurs, reload the video to replay it
+    player.playVideo();
+}
 
-      playVideo(data.video);
-    })
-    .catch(error => console.error("Error fetching recipes:", error));
-
-    
+// Function to play a specific video
+function playVideo(videoId) {
+    if (!window.YT) {
+        loadYouTubePlayer(); // Load YouTube API if not loaded
+    } else {
+        player.loadVideoById(videoId);
+    }
 }
 
 // Initialize YouTube Player
@@ -187,14 +235,3 @@ function playVideo(videoId) {
     player.loadVideoById(videoId);
   }
 }
-
-var box1 = document.getElementById('box1');
-    var box2 = document.getElementById('box2');
-
-    function syncScroll(event) {
-        if (event.target === box1) {
-            box2.scrollTop = box1.scrollTop;
-        } else if (event.target === box2) {
-            box1.scrollTop = box2.scrollTop;
-        }
-    }
